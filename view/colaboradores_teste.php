573561f54b019b58e6df067fdadc85b877912f25
Carlos <carlos.evangelista@cybernetprovedor.com>
2020-06-25 16:43:04 -0300

    Padrão de modal para visualização inserido

--- view/colaboradores_teste.php
+++ view/colaboradores_teste.php
@@ -84,13 +84,17 @@ include "../model/banco.php";
                                                 <label for="exampleInputEmail1">CEP</label>
                                                 <input type="text" class="form-control" name="grava_colaborador_cep"id="grava_colaborador_cep" maxlength="15" placeholder="CEP" />
                                             </div>
-                                            <div class="form-group col-lg-4">
+                                            <div class="form-group col-lg-2">
                                                 <label for="exampleInputEmail1">Rua</label>
                                                 <input type="text" class="form-control" name="grava_colaborador_rua"id="grava_colaborador_rua" maxlength="15" placeholder="Rua" />
                                             </div>
                                             <div class="form-group col-lg-3">
                                                 <label for="exampleInputEmail1">Bairro</label>
                                                 <input type="text" class="form-control" name="grava_colaborador_bairro"id="grava_colaborador_bairro" maxlength="15" placeholder="Bairro" />
+                                            </div>
+                                            <div class="form-group col-lg-2">
+                                                <label for="exampleInputEmail1">Nº</label>
+                                                <input type="text" class="form-control" name="grava_colaborador_numero"id="grava_colaborador_numero" maxlength="4" placeholder="Número"/>
                                             </div>
                                             <div class="form-group col-lg-3">
                                                 <label for="exampleInputEmail1">Cidade</label>
@@ -209,53 +213,44 @@ include "../model/banco.php";
                     </div>
                 </div>
                    
-                    <div class="modal fade" id="alterarModal" tabindex="-1" role="dialog" aria-labelledby="alterarModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="edita_colaboradores_modal" tabindex="-1" role="dialog" aria-labelledby="editarModalLabel" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="alterarModalLabel">Editar colaborador</h5>
+                                    <h5 class="modal-title" id="editarModalLabel">Novo colaborador</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
-                                <div class="modal-body">
-                                <form role="form" method="POST" action="../model/edita_colaboradores.php">
+                            <div class="modal-body">
+                                <form role="form" method="POST" action="../model/grava_colaboradores.php">
                                     <fieldset>
-                                    <div class="form-group">
-				                    <input type="hidden"  class="form-control"name="modalId" id="modalId" />
-				                    </div>
-                                        <div class="row">
-                                            <div class="form-group col-lg-6">
-                                                <label for="">Nome Completo</label>
-                                                <input type="text" class="form-control" name="modalnome" id="modalnome">
-                                            </div>
-                                            <div class="form-group col-lg-6">
-                                                <label for="exampleInputEmail1">Endereço</label>
-                                                <input type="text" class="form-control" name="modalendereco" id="modalendereco">
-                                            </div>
-                                        </div>
                                         <div class="row">
                                             <div class="form-group col-lg-4">
+                                                <label for="exampleInputEmail1">Nome completo</label>
+                                                <input type="text" class="form-control" name="edita_colaborador_nome" id="edita_colaborador_nome" placeholder="Insira seu nome">
+                                            </div>
+                                            <div class="form-group col-lg-4">
                                                 <label for="exampleInputEmail1">CPF</label>
-                                                <input type="text" class="form-control" name="modalcpf" id="modalcpf">
+                                                <input type="text" class="form-control" name="edita_colaborador_cpf" id="edita_colaborador_cpf" placeholder="Insira o CPF">
                                             </div>
                                             <div class="form-group col-lg-4">
                                                 <label for="exampleInputEmail1">RG</label>
-                                                <input type="text" class="form-control" name="modalrg" id="modalrg">
-                                            </div>
-                                            <div class="form-group col-lg-4">
-                                                <label for="exampleInputEmail1">Status</label>
-                                                <select name="modalativo" id="modalativo" class="form-control">
-                                                    <option value="">Selecione algo</option>
-                                                    <option value=0>Ativo</option>
-                                                    <option value=1>Desligado</option>
-                                                </select>
+                                                <input type="text" class="form-control" name="edita_colaborador_rg" id="edita_colaborador_rg" placeholder="Insira o RG">
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="form-group col-lg-6">
+                                            <div class="form-group col-lg-3">
+                                                <label for="exampleInputEmail1">Nascimento</label>
+                                                <input type="date" class="form-control" name="edita_colaborador_nascimento" id="edita_colaborador_nascimento">
+                                            </div>
+                                            <div class="form-group col-lg-3">
+                                                <label for="exampleInputEmail1">Admissão</label>
+                                                <input type="date" class="form-control" name="edita_colaborador_admissao" id="edita_colaborador_admissao">
+                                            </div>
+                                            <div class="form-group col-lg-2">
                                                 <label for="exampleInputEmail1">Sexo</label>
-                                                <select name="modalsexo" id="modalsexo"class="form-control">
+                                                <select name="edita_colaborador_sexo" id="edita_colaborador_sexo" class="form-control">
                                                     <option value="">Selecione</option>
                                                     <?php
 					                                $result_cat_post = "SELECT * FROM tb_sexo ORDER BY sexo";
@@ -266,12 +261,55 @@ include "../model/banco.php";
 				                                    ?>
                                                 </select>
                                             </div>
-                                            <div class="form-group col-lg-6">
-                                                <label for="exampleInputEmail1">Cargo</label>
-                                                <select name="modalcargo" id="modalcargo" class="form-control">
-                                                    <option value="">Selecione o cargo</option>
+                                            <div class="form-group col-lg-4">
+                                                <label for="exampleInputEmail1">E-mail</label>
+                                                <input type="text" class="form-control" name="edita_colaborador_email" id="editacolaborador_email" placeholder="Insira seu endereço">
+                                            </div>
+                                        </div>
+                                       
+                                        <div class="row">
+                                            <div class="form-group col-lg-2">
+                                                <label for="exampleInputEmail1">CEP</label>
+                                                <input type="text" class="form-control" name="edita_colaborador_cep"id="edita_colaborador_cep" maxlength="15" placeholder="CEP" />
+                                            </div>
+                                            <div class="form-group col-lg-2">
+                                                <label for="exampleInputEmail1">Rua</label>
+                                                <input type="text" class="form-control" name="edita_colaborador_rua"id="edita_colaborador_rua" maxlength="15" placeholder="Rua" />
+                                            </div>
+                                            <div class="form-group col-lg-3">
+                                                <label for="exampleInputEmail1">Bairro</label>
+                                                <input type="text" class="form-control" name="edita_colaborador_bairro"id="edita_colaborador_bairro" maxlength="15" placeholder="Bairro" />
+                                            </div>
+                                            <div class="form-group col-lg-2">
+                                                <label for="exampleInputEmail1">Nº</label>
+                                                <input type="text" class="form-control" name="edita_colaborador_numero"id="edita_colaborador_numero" maxlength="4" placeholder="Número"/>
+                                            </div>
+                                            <div class="form-group col-lg-3">
+                                                <label for="exampleInputEmail1">Cidade</label>
+                                                <input type="text" class="form-control" name="edita_colaborador_cidade"id="edita_colaborador_cidade" maxlength="15" placeholder="Cidade" />
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                        <div class="form-group col-lg-3">
+                                                <label for="exampleInputEmail1">Setor</label>
+                                                <select name="edita_colaborador_setor" name="edita_colaborador_setor" class="form-control">
+                                                    <option value="">Selecione</option>
                                                     <?php
-					                                $result_cat_post = "SELECT * FROM tb_cargo ORDER BY cargo";
+					                                $result_cat_post = "SELECT * FROM tb_setores";
+					                                $resultado_cat_post = mysqli_query($mysqli, $result_cat_post);
+					                                while($row_cat_post = mysqli_fetch_assoc($resultado_cat_post) ) {
+						                            echo '<option value="'.$row_cat_post['id_setores'].'">'.$row_cat_post['nome'].'</option>';
+					                                }
+				                                    ?>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-lg-3">
+                                                <label for="exampleInputEmail1">Cargo</label>
+                                                <select name="edita_colaborador_cargo" name="edita_colaborador_cargo" class="form-control">
+                                                    <option value="">Selecione</option>
+                                                    <?php
+					                                $result_cat_post = "SELECT * FROM tb_cargo";
 					                                $resultado_cat_post = mysqli_query($mysqli, $result_cat_post);
 					                                while($row_cat_post = mysqli_fetch_assoc($resultado_cat_post) ) {
 						                            echo '<option value="'.$row_cat_post['id_cargo'].'">'.$row_cat_post['cargo'].'</option>';
@@ -279,25 +317,33 @@ include "../model/banco.php";
 				                                    ?>
                                                 </select>
                                             </div>
-                                        </div>
-                                        <div class="row">
                                             <div class="form-group col-lg-3">
-                                                <label for="exampleInputEmail1">Telefone</label>
-                                                <input type="text" class="form-control" name="modaltelefone"id="modaltelefone" />
-                                            </div>
-                                            <div class="form-group col-lg-6">
-                                                <label for="exampleInputEmail1">E-mail</label>
-                                                <input type="email" class="form-control" name="modalemail" id="modalemail">
+                                                <label for="exampleInputEmail1">Nível</label>
+                                                <select name="edita_colaborador_nivel" name="edita_colaborador_nivel" class="form-control">
+                                                    <option value="">Selecione</option>
+                                                    <?php
+					                                $result_cat_post = "SELECT * FROM tb_categoria";
+					                                $resultado_cat_post = mysqli_query($mysqli, $result_cat_post);
+					                                while($row_cat_post = mysqli_fetch_assoc($resultado_cat_post) ) {
+						                            echo '<option value="'.$row_cat_post['id_categoria'].'">'.$row_cat_post['categoria'].'</option>';
+					                                }
+				                                    ?>
+                                                </select>
                                             </div>
                                             <div class="form-group col-lg-3">
-                                                <label for="exampleInputEmail1">Nascimento</label>
-                                                <input type="date" class="form-control" name="modalnascimento" id="modalnascimento">
+                                                <label for="exampleInputEmail1">Status</label>
+                                                <select name="edita_colaborador_status" name="edita_colaborador_status" class="form-control">
+                                                    <option value="">Selecione</option>
+                                                    <?php
+					                                $result_cat_post = "SELECT * FROM tb_ativo";
+					                                $resultado_cat_post = mysqli_query($mysqli, $result_cat_post);
+					                                while($row_cat_post = mysqli_fetch_assoc($resultado_cat_post) ) {
+						                            echo '<option value="'.$row_cat_post['id_ativo'].'">'.$row_cat_post['ativo'].'</option>';
+					                                }
+				                                    ?>
+                                                </select>
                                             </div>
                                         </div>
-                                        <div class="box-actions">
-                                            <button type="submit" class="btn btn-primary">Salvar</button>
-                                        </div>
-                                 
                                     </fieldset>
                                 </form>
                             </div>		
