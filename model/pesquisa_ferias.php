41cf039332d9d50462ecba22bc3fe051e3afd903
Carlos Monteiro <carlos.monteiro@cybernet.local>
2020-07-20 14:21:35 -0300

    Feito retorno nos inputs a partir de um select ou input padrão

---
+++ model/pesquisa_ferias.php
@@ -0,0 +1,20 @@
+<?php
+include_once "banco.php";
+
+function retorna($codigo, $mysqli){
+	$result_colaborador = "SELECT * FROM tb_colaboradores WHERE nome = '$codigo' LIMIT 1";
+	$resultado_colaborador = mysqli_query($mysqli, $result_colaborador);
+	if($resultado_colaborador->num_rows){
+		$row_colaborador = mysqli_fetch_assoc($resultado_colaborador);
+		$valores['ferias_cargo'] = $row_colaborador['cargo'];
+		$valores['ferias_setor'] = $row_colaborador['setor'];
+	}else{
+		$valores['nome_colaborador'] = 'Cargo não encontrado';
+	}
+	return json_encode($valores);
+}
+
+if(isset($_GET['codigo'])){
+	echo retorna($_GET['codigo'], $mysqli);
+}
+?>
--- view/ferias.php
+++ view/ferias.php
@@ -48,30 +48,32 @@ include ("../model/banco.php");
                             <div class="modal-body">
                                 <form role="form" method="POST">
                                     <fieldset>
-                                    <div class="input-group md-form form-sm form-2 pl-0">
-                                    <input class="form-control my-0 py-1 red-border" type="text" placeholder="Nome do colaborador" aria-label="Search">
-                                    <div class="input-group-append">
-                                        <span class="input-group-text red lighten-3" id="basic-text1"><button type="submit" class="fas fa-search text-grey"
-                                            aria-hidden="true"></button></span>
-                                    </div>
-                                    </div><br>
+                                    
+                                    <div class="form-group col-lg-13">
+                                                <label for="exampleInputEmail1">Colaborador</label>
+                                                <select name="grava_ferias_colaborador" id="grava_ferias_colaborador" class="form-control">
+                                                    <option value="">Selecione o colaborador</option>
+                                                    <?php
+					                                $result_cat_post = "SELECT * FROM tb_colaboradores";
+					                                $resultado_cat_post = mysqli_query($mysqli, $result_cat_post);
+					                                while($row_cat_post = mysqli_fetch_assoc($resultado_cat_post) ) {
+						                            echo '<option value="'.$row_cat_post['nome'].'">'.$row_cat_post['nome'].'</option>';
+					                                }
+				                                    ?>
+                                                </select>
       
                                         <div class="row">
 
-                                        <div class="form-group col-lg-4">
+                                        <div class="form-group col-lg-6">
                                                 <label for="exampleInputEmail1">Cargo</label>
-                                                <input type="text" class="form-control" name="grava_ferias_setor" id="grava_ferias_setor" disabled="">
+                                                <input type="text" class="form-control" name="grava_ferias_cargo" id="grava_ferias_cargo">
                                         </div>
 
-                                            <div class="form-group col-lg-4">
+                                            <div class="form-group col-lg-6">
                                                 <label for="exampleInputEmail1">Setor</label>
-                                                <input type="text" class="form-control" name="grava_ferias_setor" id="grava_ferias_setor" disabled="">
+                                                <input type="text" class="form-control" name="grava_ferias_setor" id="grava_ferias_setor">
                                             </div>
 
-                                            <div class="form-group col-lg-4">
-                                                <label for="exampleInputEmail1">Salário</label>
-                                                <input type="text" class="form-control" name="grava_ferias_setor" id="grava_ferias_setor" disabled="">
-                                            </div>
                                             
                                             <div class="form-group col-lg-3">
                                                 <label for="exampleInputEmail1">Abono pecuniário</label>
@@ -137,10 +139,25 @@ include ("../model/banco.php");
                         documento.value += texto.substring(0,1);
             }
         }
-</script>
+        </script>
+		
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+        <script type='text/javascript'>
+			$(document).ready(function(){
+				$("select[name='grava_ferias_colaborador']").blur(function(){
+					var $ferias_cargo = $("input[name='grava_ferias_cargo']");
+					var $ferias_setor = $("input[name='grava_ferias_setor']");
+					$.getJSON('../model/pesquisa_ferias.php',{ 
+						codigo: $( this ).val() 
+					},function( json ){
+						$ferias_cargo.val( json.ferias_cargo );
+						$ferias_setor.val( json.ferias_setor );
+					});
+				});
+			});
+		</script>
 
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         </html>
-
